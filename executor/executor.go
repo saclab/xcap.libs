e136ac69226baa7cc2890a0f1383293a3096f8df
Siddhant Rath <sid@tamu.edu>
2020-03-20 13:53:08 -0500

    changed run and shellout to accept extar params as a map

--- executor/executor.go
+++ executor/executor.go
@@ -5,11 +5,11 @@ package executor
 
 import (
 	"bytes"
-	"fmt"
 	"io"
-	"log"
 	"os"
 	"os/exec"
+
+	"github.com/saclab/xcap.libs/logger"
 )
 
 // Run : Run a function and return the std output and error
@@ -17,24 +17,29 @@ import (
 // Executes the binary with the given flags and
 // returns stdOut (string) and stdErr (string)
 // func Run( string,  string) (string, string)
-func Run(binaryPath string, params string) (string, string) {
-	fmt.Println("[+++++ xcap.libs.executor.Run] Start Run for ", binaryPath, params)
+// NOTE: Maintains a 'drop in replacement' relationship with function Shellout
+func Run(binaryPath string, params string, extra map[string]string) (string, string) {
+	logger.InitD()
+	logger.Info.Println("(xcap.libs.executor.Run) Start Run for ", binaryPath, params)
 
 	cmd := exec.Command(binaryPath, params)
+	if val, ok := extra["dir"]; ok {
+		cmd.Dir = val
+	}
+
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
 	err := cmd.Run()
 	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+		logger.Error.Println("(xcap.libs.executor.Run) cmd.Run() failed with : ", err)
 	}
 
 	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
 
-	fmt.Println("[+++++ xcap.libs.executor.Run] End Run for ", binaryPath, params)
+	logger.Info.Println("(xcap.libs.executor.Run)  End Run for ", binaryPath, params)
 	return outStr, errStr
-
 }
 
 // Shellout : Run and stream the output to std display, also retuen the std out and err
@@ -42,21 +47,27 @@ func Run(binaryPath string, params string) (string, string) {
 // Executes the binary with the given flags and
 // returns stdOut (string) and stdErr (string)
 // func Shellout( string,  string) (string, string)
-func Shellout(binaryPath string, params string) (string, string) {
-	fmt.Println("[+++++ xcap.libs.executor.Shellout] Start Shellout for ", binaryPath, params)
+// NOTE: Maintains a 'drop in replacement' relationship with function Run
+func Shellout(binaryPath string, params string, extra map[string]string) (string, string) {
+	logger.InitD()
+	logger.Info.Println("(xcap.libs.executor.Shellout) Start Shellout for ", binaryPath, params)
 
 	cmd := exec.Command(binaryPath, params)
+	if val, ok := extra["dir"]; ok {
+		cmd.Dir = val
+	}
+
 	var stdoutBuf, stderrBuf bytes.Buffer
 	cmd.Stdout = io.MultiWriter(os.Stdout, &stdoutBuf)
 	cmd.Stderr = io.MultiWriter(os.Stderr, &stderrBuf)
 
 	err := cmd.Run()
 	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+		logger.Error.Println("(xcap.libs.executor.Shellout) cmd.Run() failed with : ", err)
 	}
 
 	outStr, errStr := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
 
-	fmt.Println("[+++++ xcap.libs.executor.Shellout] End Shellout for ", binaryPath, params)
+	logger.Info.Println("(xcap.libs.executor.Shellout)  End Shellout for ", binaryPath, params)
 	return outStr, errStr
 }
